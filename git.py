352330d1525db9517f7ae6fb6d89d0c5a02d95f6
Anirban Chatterjee <ani.kol2099@gmail.com>
2025-03-25 01:17:34 +0530

    git

--- git.py
+++ git.py
@@ -1,10 +1,10 @@
 import os , random
 
-for i in range(500):
+for i in range(100):
     d = str(i) + 'days ago'
-    rand = random.randrange(10, 11)
+    rand = random.randrange(2, 3)
     with open('test.txt','a') as file:
         file.write(d + '\n')
         os.system('git add test.txt')
-        os.system('git commit --date=" 2024-' +str(rand)+ '-'+d+'" -m 1')
+        os.system('git commit --date=" 2025-' +str(rand)+ '-'+d+'" -m 1')
         os.system('git push -u origin main -f')
